74b676fafddd13fd5de11c9b5a296af7e85aad69
Calebkillom <Calebkillom@gmail.com>
2024-09-01 17:31:39 +0300

    task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,11 +1,11 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-  
+
   try {
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    queue.push(`Error: ${error.message}`);
+    queue.push(error.toString());
   } finally {
     queue.push('Guardrail was processed');
   }
