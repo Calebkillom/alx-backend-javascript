257afa7e2733a1bdb56f58fde1942115ac94e6b7
Calebkillom <Calebkillom@gmail.com>
2025-01-04 09:55:18 +0300

    Task 1 corrections

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,17 @@
-process.stdout.write('Welcome to ALX, what is your name?\n');
-
-const inputHandler = (userInput) => {
-  const name = userInput.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  process.exit();
-};
-
-const exitHandler = () => {
-  process.stdout.write('This important software is now closing\n');
-};
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
-  process.stdin.once('data', inputHandler);
+  process.stdin.on('data', (data) => {
+    const input = data.toString();
+    process.stdout.write(`Your name is: ${input}`);
+    process.exit();
+  });
 } else {
-  process.stdin.once('data', inputHandler);
-  process.on('exit', exitHandler);
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
 }
