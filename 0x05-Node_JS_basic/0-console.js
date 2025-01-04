a5c7e7454cb3b254742af93ef7cd79c44990ac4d
Calebkillom <Calebkillom@gmail.com>
2025-01-04 09:36:46 +0300

    task 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Function to print a string argument to STDOUT
+ * @param {string} message - The message to display
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
